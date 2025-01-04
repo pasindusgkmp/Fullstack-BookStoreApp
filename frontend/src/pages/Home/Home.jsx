ac071a84093f56ecfffe1a1833526546f8400d53
CharithaMadhumal <charithamadhumal174@gmail.com>
2025-01-05 00:37:23 +0530

    done footer section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from './Banner'
 import TopSellers from './TopSellers'
 import Recommended from './Recommended'
 import News from './News'
+import Footer from '../../components/Footer'
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
         <TopSellers/>
         <Recommended/>
         <News/>
+        
     </>
   )
 }
