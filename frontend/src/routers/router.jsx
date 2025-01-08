f949970a8a4d352404a32712f8735772afc65316
Pasidu Sathsara <pasindugunasekara889@gmail.com>
2025-01-09 02:18:44 +0530

    Cart page,Remove from Cart,Clear Cart done

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -3,6 +3,8 @@ import App from "../App";
 import Home from "../pages/Home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import CartPage from "../pages/books/CartPage";
+import CheckoutPage from "../pages/books/CheckoutPage";
 
 const router = createBrowserRouter([
     {
@@ -28,6 +30,17 @@ const router = createBrowserRouter([
             {
                 path: "/register",
                 element: <Register/>
+            }
+            
+            ,
+            {
+                path: "/cart",
+                element: <CartPage/>
+            }
+            ,
+            {
+                path: "/checkout",
+                element: <CheckoutPage/>
             }
             
                 
