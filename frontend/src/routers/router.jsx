a695e3101b16991992c74e3df395fe7bc6700e28
CharithaMadhumal <charithamadhumal174@gmail.com>
2025-01-14 15:59:15 +0530

    done creating bookAPI using RTK query

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -5,6 +5,7 @@ import Login from "../components/Login";
 import Register from "../components/Register";
 import CartPage from "../pages/books/CartPage";
 import CheckoutPage from "../pages/books/CheckoutPage";
+import SingleBook from "../pages/books/SingleBook";
 
 const router = createBrowserRouter([
     {
@@ -41,6 +42,10 @@ const router = createBrowserRouter([
             {
                 path: "/checkout",
                 element: <CheckoutPage/>
+            },
+            {
+                path: "/books/:id",
+                element: <SingleBook/>
             }
             
                 
