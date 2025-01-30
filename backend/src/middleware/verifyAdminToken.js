6839d842722c3a17db3768cdb0edd7734b33ba7f
CharithaMadhumal <charithamadhumal174@gmail.com>
2025-01-30 10:59:08 +0530

    done admin router and dashboard login

--- backend/src/middleware/verifyAdminToken.js
+++ backend/src/middleware/verifyAdminToken.js
@@ -3,6 +3,9 @@ const JWT_SERVER_SECRET = process.env.JWT_SERVER_SECRET
 
 const verifyAdminToken =(req,res,next)=>{
     const token = req.headers['authorization']?.split(' ')[1];
+
+    console.log(token);
+    
     if(!token){
         return res.status(401).json({message:"Access denied.No token provided"});
     }
